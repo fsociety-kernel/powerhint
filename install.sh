8da01a9fcab99cf6d006e78e1f82d3384711a7f2
kingbri <bdashore3@gmail.com>
2019-12-20 17:40:18 -0500

    init module
    Doesn't work on Magisk <19000
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- install.sh
+++ install.sh
@@ -122,9 +122,10 @@ REPLACE="
 # Set what you want to display when installing your module
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "*****************************************"
+  ui_print "     Pixel 4 Powerhint install script    "
+  ui_print "           by kingbri@kingdevel          "
+  ui_print "*****************************************"
 }
 
 # Copy/extract your module files into $MODPATH in on_install.
